3f3ad9e484f6d63538584e6fa2cf29680a9177b0
Ezerath <44700508+Ezerath@users.noreply.github.com>
2019-10-19 00:14:48 +0300

    Создал класс детали

---
+++ Detail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuttingApp
+{
+
+    public class Detail
+    {
+        public string Name { get; private set; }
+        public int Length { get; private set; }
+        public int Width { get; private set; }
+        public Detail(string name,int length,int width)
+        {
+            Name = name;
+            Length = length;
+            Width = width;
+        }
+    }
+}
