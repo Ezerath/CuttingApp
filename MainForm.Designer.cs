d8448b60972a66300497898585ddf13b757d53f8
Ezerath <44700508+Ezerath@users.noreply.github.com>
2019-10-19 01:21:37 +0300

    Разобрался с масштабированием

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,20 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(40, 23);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(680, 399);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CuttingApp";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -12,9 +12,30 @@ namespace CuttingApp
 {
     public partial class MainForm : Form
     {
+        Detail detail;
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CreateRect(PaintEventArgs e)
+        {
+            detail = new Detail("Material", 2750, 1830);
+            Pen pen = new Pen(Color.Black);
+            Rectangle rect = new Rectangle(10, 10, detail.Length, detail.Width);
+            //rect.Size = new Size(detail.Length, detail.Width);
+            e.Graphics.ScaleTransform(0.1f, 0.1f);
+            e.Graphics.DrawRectangle(pen, rect);
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            CreateRect(e);
         }
     }
 }
